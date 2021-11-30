6c1eaca343fcf2b19bf4729b738cc18457999327
Bor Buh <bor1.buh@gmail.com>
2021-12-01 00:00:35 +0100

    Initial Commit

---
+++ BBle/BBleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBleApp.swift
+//  BBle
+//
+//  Created by Bor Buh on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BBleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
